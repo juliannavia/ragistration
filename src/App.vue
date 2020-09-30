74d8592020518e48f7bd88b8cf12da35728a7ea9
juliannavia <juliannavia157@hotmail.es>
2020-09-30 09:22:42 -0500

    asad

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
                   <div id="itemsNavBar" class="col-sm-4">
                     <li class="nav-item">
                       <a class="nav-link" href="https://pikinglish.com"
-                        >SITIO WEBcommit2</a
+                        >SITIO WEBcommit3</a
                       >
                     </li>
                   </div>
